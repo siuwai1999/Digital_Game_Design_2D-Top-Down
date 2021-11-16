ce4e167d5ab62f9d2051af444b5337ffb4d01a79
siuwai1999 <SIUWAI1999@outlook.com>
2021-11-16 16:07:56 +0800

    1611

--- Assets/PlayerCtrl.cs
+++ Assets/PlayerCtrl.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour
 {
     public float moveSpeed = 0.01f;
+    public Animator RubyAnim;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        RubyAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 rubyMove = transform.position;
-        rubyMove.x = rubyMove.x + Input.GetAxis("Horizontal") * moveSpeed;
-        rubyMove.y = rubyMove.y + Input.GetAxis("Vertical") * moveSpeed;
+        float PlayerMoveX = Input.GetAxis("Horizontal");
+        float PlayerMoveY = Input.GetAxis("Vertical");
+        RubyAnim.SetFloat("MoveX", PlayerMoveX);
+        RubyAnim.SetFloat("MoveY", PlayerMoveY);
+        rubyMove.x += PlayerMoveX * moveSpeed;
+        rubyMove.y += PlayerMoveY * moveSpeed;
         transform.position = rubyMove;
+
+        if (PlayerMoveX == 0 && PlayerMoveY == 0 )
+        {
+            RubyAnim.SetTrigger("Idle");
+        };
     }
 }
